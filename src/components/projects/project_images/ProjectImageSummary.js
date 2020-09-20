0b77b4b8cc22f172a6b3481fdacdf3ffdb1398ac
Jade <jademmb@gmail.com>
2020-09-20 17:34:13 +1200

    Added image zooming

--- src/components/projects/project_images/ProjectImageSummary.js
+++ src/components/projects/project_images/ProjectImageSummary.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import ModalImage from "react-modal-image";
 
 const ProjectImageSummary = ({ image }) => {
 
     return (
         <div className="card hoverable">
-            <div className="card-image">
-                <p><img className="responsive-img" src={image.url} alt={image.photoName} /></p>
+            <div className="card-image" id="imageClick">
+                <ModalImage
+                    small={image.url}
+                    medium={image.url}
+                    alt={image.photoName}
+                    />
             </div>
         </div>
     )
